50ab5f1ee37ec5e118979908e0cf9036412da8cc
sxk <269531918@qq.com>
2017-03-16 17:04:27 +0800

    update Message

--- First/LearnCSMode/Message.java
+++ First/LearnCSMode/Message.java
@@ -1,5 +1,39 @@
+package common;
+import java.io.Serializable;
 
+public class Message implements Serializable{
+	
+	private int random1;
+	private int random2;
+	private String timer;
+	
+	public Message() {
 
-public class  Message implements Serializable {
-    private int
+	}
+	
+	public int getRandom1() {
+		return random1;
+	}
+
+	public void setRandom1(int random1) {
+		this.random1 = random1;
+	}
+
+	public int getRandom2() {
+		return random2;
+	}
+
+	public void setRandom2(int random2) {
+		this.random2 = random2;
+	}
+
+	public String getTimer() {
+		return timer;
+	}
+
+	public void setTimer(String timer) {
+		this.timer = timer;
+	}
+
+	
 }
