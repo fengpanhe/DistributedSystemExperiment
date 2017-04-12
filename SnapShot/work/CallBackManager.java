f709cc8b52ae1003784a975d76789ac6641f1aa1
sxk <269531918@qq.com>
2017-04-12 12:47:50 +0800

    456

--- SnapShot/work/CallBackManager.java
+++ SnapShot/work/CallBackManager.java
@@ -1,30 +1,14 @@
 package work;
 
 public class CallBackManager {
-<<<<<<< HEAD
-	private static ICallBack callBackp;
-=======
+
 	private static ICallBack callBack;
->>>>>>> cd237ef5ef1384b4238b029c950007393b77bfbb
 	private static ICallBack callBackc;
 	
 	public static ICallBack getCallBackc() {
 		return callBackc;
 	}
 
-<<<<<<< HEAD
-	public static void setCallBackC(ICallBack callBackc) {
-		CallBackManager.callBackc = callBackc;
-	}
-
-	public static void setCallBackP(ICallBack call) {
-		CallBackManager.callBackp = call;
-	}
-	
-	public static ICallBack getCallBackP() {
-		return callBackp;
-	}
-=======
 	public static void setCallBackc(ICallBack callBackc) {
 		CallBackManager.callBackc = callBackc;
 	}
@@ -37,5 +21,4 @@ public class CallBackManager {
 		CallBackManager.callBack = callBack;
 	}
 	
->>>>>>> cd237ef5ef1384b4238b029c950007393b77bfbb
 }
